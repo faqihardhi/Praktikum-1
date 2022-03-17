91b4885e22189b853936d4f9698b49e47c84358f
ardhifaqih <ardhifq10@gmail.com>
2022-03-17 09:20:32 +0700

    untuk kondisional (true/false), String untuk array karakter, Array yang merupakan koleksi data dengan tipe data yang homogen, Collection dan Range.

---
+++ src/main/kotlin/7_TipeData.kt
@@ -0,0 +1,34 @@
+fun main(args: Array<String>) {
+    //Pemberian nilai minByte
+    val minByte: Byte = -128
+    //Pemberian nilai maxByte
+    val maxByte: Byte = 127 // 8bit
+    //Pemberian nilai minShort
+    val minShort: Short = -32768
+    //Pemberian nilai maxShort
+    val maxShort: Short = 32767 // 16bit
+    //Pemberian nilai minInt
+    val minInt: Int = -2147483648
+    //Pemberian nilai maxInt
+    val maxInt: Int = 2147483647 // 32bit
+    //Pemberian nilai minLong
+    val minLong: Long = -9223372036854775807
+    //Pemberian nilai maxLong
+    val maxLong: Long = 9223372036854775807 // 64bit
+    //menjalankan perintah val minByte: Byte
+    println("minByte:" + minByte)
+    //menjalankan perintah val maxByte: Byte
+    println("maxByte:" + maxByte)
+    //menjalankan perintah val minShort: Byte
+    println("minShort:" + minShort)
+    //menjalankan perintah val maxShort: Byte
+    println("maxShort:" + maxShort)
+    //menjalankan perintah val minInt: Byte
+    println("minInt:" + minInt)
+    //menjalankan perintah val maxInt: Byte
+    println("maxInt:" + maxInt)
+    //menjalankan perintah val minLong: Byte
+    println("minLong:" + minLong)
+    //menjalankan perintah val maxLong: Byte
+    println("maxLong:" + maxLong)
+}
