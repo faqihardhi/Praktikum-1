32005292ab1171ffcb6b891be84a84e59faa0755
ardhifaqih <ardhifq10@gmail.com>
2022-03-17 09:19:09 +0700

    Variabel adalah sebuah tempat/wadah didalam memori komputer untuk menyimpan data seperti data nama pengguna, kata sandi, data waktu, jumlah makanan dan lain-lain sebagainya.

---
+++ src/main/kotlin/5_Variabel.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    //menampilkan nilai dari dynamic bahwa variabel dynamic bertipe int
+    var dynamicVar = 2
+    println("Contoh dynamic variabel = " + dynamicVar)
+    //menampilkan nilai dari static bahwa variabel static bertipe char
+    var staticVar: Char = 'A'
+    println("Contoh static variabel = " + staticVar)
+}
